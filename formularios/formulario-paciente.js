c7f07cdd60e120c9a00015ad3119f9cd922849fe
alvarogithub2024 <alvaro.fer.salas@gmail.com>
2025-09-24 19:26:30 -0300

    Reorder imports in formulario-paciente.js

--- formularios/formulario-paciente.js
+++ formularios/formulario-paciente.js
@@ -3,8 +3,7 @@
  */
 
 import { showNotification } from '../utilidades/notificaciones.js';
-import { validateRUT, isValidEmail, formatRUT, formatPhoneNumber } from '../utilidades/validaciones.js';
-
+import { isValidEmail, validateRUT, formatRUT } from '../utilidades/validaciones.js';
 // IMPORTS CORREGIDOS - SIN getServerTimestamp
 import { getFirestore } from '../configuracion/firebase.js';
 
