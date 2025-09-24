39895d962dfd65596a90475f84f819ff326f106c
alvarogithub2024 <alvaro.fer.salas@gmail.com>
2025-09-24 14:15:44 -0300

    Update formulario-paciente.js

--- formularios/formulario-paciente.js
+++ formularios/formulario-paciente.js
@@ -4,8 +4,7 @@
  */
 
 import { showNotification } from '../utilidades/notificaciones.js';
-import { validateStep, validateRUT } from './validaciones.js';
-import { isValidEmail } from '../utilidades/validaciones.js'; 
+import { validateRUT, isValidEmail , validateStep} from '../utilidades/validaciones.js';
 import { setupAutoSave, saveFormDraft, loadFormDraft, resetForm } from './autoguardado.js';
 import { handlePatientFormSubmit, handleInformationOnlySubmit } from '../solicitudes/gestor-solicitudes.js';
 
