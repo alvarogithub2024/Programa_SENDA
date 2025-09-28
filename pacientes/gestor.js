b31b5489c12b52ba21273f341243eeb08816424f
alvarogithub2024 <alvaro.fer.salas@gmail.com>
2025-09-28 06:07:35 -0300

    Update gestor.js

--- pacientes/gestor.js
+++ pacientes/gestor.js
@@ -119,4 +119,3 @@ function obtenerPacientePorId(pacienteId, callback) {
 window.guardarPaciente = guardarPaciente;
 window.buscarPacientes = buscarPacientes;
 window.obtenerPacientePorId = obtenerPacientePorId;
-
