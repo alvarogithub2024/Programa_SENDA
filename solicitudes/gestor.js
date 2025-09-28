eb6fcae492174b7423468a31d0def0e1f1ae84e1
alvarogithub2024 <alvaro.fer.salas@gmail.com>
2025-09-27 22:33:51 -0300

    Update gestor.js

--- solicitudes/gestor.js
+++ solicitudes/gestor.js
@@ -123,7 +123,7 @@ function renderSolicitudesTable() {
             tableBody.innerHTML = `<tr><td colspan="8" class="text-center" style="padding:48px;">No hay solicitudes</td></tr>`;
             return;
         }
-        
+
         const rows = filteredSolicitudesData.map(solicitud => {
             let estadoHtml;
             if (solicitud.origen === 'informacion') {
@@ -176,8 +176,6 @@ function renderSolicitudesTable() {
                 </div>
             `;
 
-      const rows = filteredSolicitudesData.map(solicitud => {
-            // ... (restante igual arriba)
             return `
                 <tr class="solicitud-row" data-solicitud-id="${solicitud.id}">
                     <td>
